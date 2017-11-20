0649c65d44b95f8d0518c7a02a76bbc368ce132a
Avery Broderick <avery.c.broderick@gmail.com>
2017-11-20 14:16:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-console.log(chocolateBars);
+function addElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+}
