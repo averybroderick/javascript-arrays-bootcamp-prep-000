7589b5ed07be6c880d5f9bfd20443f4fab044c98
Avery Broderick <avery.c.broderick@gmail.com>
2017-11-20 14:32:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  var pushedArr = [array, ... element];
+  var pushedArr = [array, ...element];
   return pushedArr;
 }
 
