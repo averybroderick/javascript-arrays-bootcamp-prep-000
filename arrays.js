4b3fb740d79ae7dba3ce9acff77686bed1f6d7a9
Avery Broderick <avery.c.broderick@gmail.com>
2017-11-20 14:24:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,5 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
   var unshiftedArr = [element, ...array];
+  return unshiftedArr;
 }
