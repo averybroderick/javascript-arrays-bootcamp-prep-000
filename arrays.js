1acfe5e178abd8b7dea7b3a4666fc11217279306
Avery Broderick <avery.c.broderick@gmail.com>
2017-11-20 14:37:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,6 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  var shiftedArr = array.shift();
+  var shiftedArr = [...array];
+  shiftedArr.shift();
   return shiftedArr;
 }
